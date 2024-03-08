759517ffa29616a1575b2f89fe86c898ea324927
fu8ionDiego <fu8ion@hotmail.com>
2024-03-08 16:04:30 -0600

    Added some progress on the equalSidesOfAnArray algorithm

--- equalsSidesOfAnArray.js
+++ equalsSidesOfAnArray.js
@@ -1,14 +1,26 @@
 const list = [1, 8, 7, 6, 9, 3, 2, 5];
-let counter = 0;
-let n1 = -1;
+let counter = 1;
+let n1 = 0;
 let n2 = -1;
-do {
+console.log(list.length);
+for (i = 1; i < list.length - 2; i++) {
+  console.log("ingreso al do");
+
   for (i = counter; i < list.length; i++) {
-    for (i = 1; i < counter; i++) {
-      console.log("primer for");
+    console.log("ingreso al for 1");
+    for (i = 0; i < counter; i++) {
+      n1 = n1 + list[i] + list[counter];
+      counter++;
+      console.log(n1);
+    }
+
+    for (i = counter + 1; i < counter; i++) {
+      console.log("segundo for");
+      n2 = list[i + 1] + list[counter];
     }
   }
-  counter = +1;
-} while (n1 != n2);
+
+  console.log(`n1: ${n1} and ${n2}`);
+}
 
 console.log(n1);
